a28e936001f80ce743cc79b159bfc18e20ecc432
vanderlyle <24830324+vanderlyle@users.noreply.github.com>
2022-06-06 13:39:03 +0200

    Add untested customization steps

--- chroot-script.sh
+++ chroot-script.sh
@@ -8,16 +8,16 @@ hwclock --systohc
 systemctl enable systemd-timesyncd # untested
 
 # Localization
-#echo -e "en_US.UTF-8 UTF-8\nhu_HU.UTF-8 UTF-8" > /etc/locale.gen # untested
-sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
-sed -i 's/#hu_HU.UTF-8 UTF-8/hu_HU.UTF-8 UTF-8/g' /etc/locale.gen # for LC_TIME
+echo -e "en_US.UTF-8 UTF-8\nhu_HU.UTF-8 UTF-8" > /etc/locale.gen # untested (hu for LC_TIME)
+#sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+#sed -i 's/#hu_HU.UTF-8 UTF-8/hu_HU.UTF-8 UTF-8/g' /etc/locale.gen # for LC_TIME
 locale-gen
 echo -e "LANG=en_US.UTF-8\nLC_TIME=hu_HU.UTF-8" > /etc/locale.conf
 echo KEYMAP=hu > /etc/vconsole.conf
 
 # Network configuration
-echo testarch > /etc/hostname
-echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\ttestarch.localdomain\ttestarch" > /etc/hosts
+echo laptop > /etc/hostname
+echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\tlaptop.localdomain\tlaptop" > /etc/hosts
 pacman -S networkmanager --needed --noconfirm || error_exit "Error: Could not install additional packages."
 systemctl enable NetworkManager
 
@@ -38,4 +38,9 @@ bootctl install --no-variables || error_exit "Error: Could not install 'bootctl'
 cp /usr/share/systemd/bootctl/arch.conf /boot/loader/entries/arch.conf
 sed -i '0,/options/s/options.*/options root=PARTUUID='"$(blkid -s PARTUUID -o value /dev/sda3)"' rw quiet/' /boot/loader/entries/arch.conf
 echo -e "timeout\tmenu-force\ndefault\tarch.conf\tmax\neditor\tno" > /boot/loader/loader.conf
-pacman -S vi vim git wget curl --needed --noconfirm || error_exit "Error: Could not install additional packages."
+
+# UNTESTED - Customization
+# Install packages
+pacman -S vi vim git wget curl --needed --noconfirm || error_exit "Error: Could not install git wget curl packages."
+pacman -S xf86-video-nouveau mesa --needed --noconfirm || error_exit "Error: Could not install xf86-video-nouveau mesa packages."
+xorg i3-gaps i3status i3lock rxvt-unicode xorg-xrdb zsh zsh-completions lightdm lightdm-gtk-greeter --needed --noconfirm || error_exit "Error: Could not install additional packages."
